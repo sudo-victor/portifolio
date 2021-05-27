16ed8b2c1b95663057eff99672aa99d1c4c743b2
sudo-victor <victor.souto@hotmail.com>
2021-05-27 17:59:55 -0300

    fix z-index bug

--- src/components/CvButton/index.tsx
+++ src/components/CvButton/index.tsx
@@ -4,7 +4,7 @@ import styles from './cv.module.scss';
 export default function CvButton() {
   return (
     <section className={styles.cvComponent} id="cv">
-        <a href="curriculo.pdf" download className={styles.cv}>
+        <a href="/download/curriculo.pdf" download className={styles.cv}>
           baixar cv
         </a>
     </section>

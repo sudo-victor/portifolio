5e80896b0f4d6aba8db5edfa7cd9d2a93a041fa6
sudo-victor <victor.souto@hotmail.com>
2021-06-02 10:42:52 -0300

    chore: add keywords meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Signika:wght@700&display=swap" rel="stylesheet" />
           <link rel="icon" href="/favicon.png" />
+          <meta name="keywords" content="HTML, CSS, JavaScript, Programador Jr, Programador Javascript Jr, Programador Fullstack, Programador Frontend, Programador Backend, TI, Programador"/>
         </Head>
 
         <body>

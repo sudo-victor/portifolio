04cddbca41b53d8f2052a5707b5d827ead6fc9ef
sudo-victor <victor.souto@hotmail.com>
2021-06-02 10:50:10 -0300

    fix: SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,14 @@ import Document, { Html, Head, Main, NextScript} from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Signika:wght@700&display=swap" rel="stylesheet" />
           <link rel="icon" href="/favicon.png" />
           <meta name="keywords" content="HTML, CSS, JavaScript, Programador Jr, Programador Javascript Jr, Programador Fullstack, Programador Frontend, Programador Backend, TI, Programador"/>
+          <link rel="canonical" href="http://www.victorsouto.com.br/" />    
+
         </Head>
 
         <body>
